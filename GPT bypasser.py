2539bb2745a0eda9155d40e6afe939584fe49558
Ranged766 <45710265+Ranged766@users.noreply.github.com>
2023-03-11 11:56:35 +0100

    hi hi

---
+++ GPT bypasser.py
@@ -0,0 +1,23 @@
+import openai
+from key import *
+#import moonlander from moonhole
+
+#Flagged account ? api_key ?
+#Flagged HWID ?
+#Flagged ip ?
+
+openai.api_key = API_KEY
+model_engine = "text-davinci-003"
+max_tokens = 1024
+while True:
+    prompt = input("Enter your text: ")
+    completion = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=max_tokens,
+    temperature=0.2,
+    top_p=0.5,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    print(completion.choices[0].text)
